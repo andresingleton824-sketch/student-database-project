bef55848c5e4819cdf25ae1b9fe6eb112ff004e2
andresingleton824-sketch <andresingleton824@gmail.com>
2026-05-05 20:16:13 -0500

    First part

---
+++ Student_database.py
@@ -0,0 +1,38 @@
+if __name__ == "__main__":
+ 
+    # Sample data for testing (your partner will provide this from her module)
+    sample_students = [
+        {
+            "student_id": 1001,
+            "first_name": "Emma",
+            "last_name": "Johnson",
+            "grade": "A",
+            "major": "Computer Science",
+            "email": "emma.j@school.edu",
+            "phone_number": "612-555-0101"
+        },
+        {
+            "student_id": 1002,
+            "first_name": "Liam",
+            "last_name": "Martinez",
+            "grade": "B+",
+            "major": "Business",
+            "email": "liam.m@school.edu",
+            "phone_number": "651-555-0188"
+        },
+        {
+            "student_id": 1003,
+            "first_name": "Sophia",
+            "last_name": "Nguyen",
+            "grade": "A-",
+            "major": "Biology",
+            "email": "sophia.n@school.edu",
+            "phone_number": "763-555-0234"
+        }
+    ]
+ 
+    main_menu(sample_students)
+# ============================================================
+# Add a diplay and menu
+# Author: Andre
+# ============================================================
